1462bc68e853da89ac4b984dc03bcff8f319978d
gabrieltaets <gabrieltaets@gmail.com>
2017-10-28 07:05:58 -0200

    Adicionado consulta por disciplina

--- sigaabot.py
+++ sigaabot.py
@@ -10,22 +10,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
 import time
-
 updater = Updater(token='')
 
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 updater.start_polling()
 
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except Exception as inst:
+    	print(type(inst))
+    	print(inst.args)
+    	print(inst)
+
+dispatcher.add_error_handler(error_callback)
+
 def start(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="Olá, eu sou o SIGAA Bot!\nDigite /notas usuario senha e eu lhe enviarei suas notas!\nNão se preocupe - seus dados não são guardados!\nSe você gosta do SIGAA Bot, por favor deixe sua avaliação: https://telegram.me/storebot?start=SIGAA_Bot")
+	bot.send_message(chat_id=update.message.chat_id, text="Olá, eu sou o SIGAA Bot, criado por Gabriel Taets!\nComandos:\n/start - exibe esta mensagem\n/help - exibe mensagem de ajuda\n/notas <usuario> <senha> - obter suas notas de todas as disciplinas\n/disc <sigla> <usuario> <senha> - obter notas da disciplina de sigla inserida\nNão se preocupe - seus dados não são guardados!\nSe você gosta do SIGAA Bot, por favor deixe sua avaliação: https://telegram.me/storebot?start=SIGAA_Bot")
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 def notas(bot, update, args):
 	if(len(args) != 2):
-		bot.send_message(chat_id=update.message.chat_id, text="Ops! A sintaxe deve ser /notas usuario senha")
+		bot.send_message(chat_id=update.message.chat_id, text="Ops! A sintaxe deve ser: /notas <usuario> <senha>")
 		return
 	USERLOGIN = args[0]
 	USERPWD = args[1]
@@ -45,16 +54,78 @@ def notas(bot, update, args):
 		pwd = driver.find_element_by_name("user.senha")
 		pwd.send_keys(USERPWD)
 		pwd.send_keys(Keys.ENTER)
-		elem = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, ("ThemeOfficeMainItem"))))
+		elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.CLASS_NAME, ("ThemeOfficeMainItem"))))
 		elem.click()
 		notas = driver.find_element_by_class_name("ThemeOfficeMenuItemText")
 		notas.click()
-		driver.save_screenshot(""+USERLOGIN+".png")
-		bot.send_photo(chat_id=update.message.chat_id, photo=open(""+USERLOGIN+".png","rb"), caption="Aqui estão suas notas!")
-		os.remove(""+USERLOGIN+".png")
+		driver.save_screenshot("img/"+USERLOGIN+".png")
+		bot.send_photo(chat_id=update.message.chat_id, photo=open("img/"+USERLOGIN+".png","rb"), caption="Aqui estão suas notas!")
 	except:
+		print(type(e))
+		print(e.args)
+		print(e)
 		bot.send_message(chat_id=update.message.chat_id, text="Sinto muito, não consegui recuperar suas notas!\nTalvez seus dados estejam incorretos, ou o SIGAA está fora do ar!")
 	driver.quit()
 
 notas_handler = CommandHandler('notas', notas, pass_args=True)
 dispatcher.add_handler(notas_handler)
+
+def disc(bot, update, args):
+	if(len(args) != 3):
+		bot.send_message(chat_id=update.message.chat_id, text="Ops! A sintaxe deve ser: /disc sigla <usuario> <senha>")
+		return
+	DISC = args[0].upper()
+	USERLOGIN = args[1]
+	USERPWD = args[2]
+	if(len(USERLOGIN) != 11):
+		bot.send_message(chat_id=update.message.chat_id, text="Ops! Parece que seu login não é válido! Lembre-se: seu login é o seu CPF!")
+		return
+	bot.send_message(chat_id=update.message.chat_id, text="Aguarde enquanto pego suas notas!")
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument("--incognito")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--window-size=1000,1000")
+	driver = webdriver.Chrome(chrome_options=chrome_options)
+	try:
+		driver.get("https://sigaa.unifei.edu.br/sigaa/verTelaLogin.do")
+		login = driver.find_element_by_name("user.login")
+		login.send_keys(USERLOGIN)
+		pwd = driver.find_element_by_name("user.senha")
+		pwd.send_keys(USERPWD)
+		pwd.send_keys(Keys.ENTER)
+		elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.ID, ("form_acessarTurmaVirtual:turmaVirtual"))))
+		elem.click()
+		elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.ID, ("formAcoesTurma:botaoTrocarTurma"))))
+		elem.click()
+		try:
+			elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, (""+DISC))))
+			elem.click()
+		except:
+			bot.send_message(chat_id=update.message.chat_id, text="Não consegui encontrar essa disciplina!");
+		else:
+			elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.CLASS_NAME, ("itemMenuHeaderAlunos"))))
+			elem.click()
+			elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.LINK_TEXT, ("Ver Notas"))))
+			elem.click()
+			try:
+				elem = WebDriverWait(driver, 25).until(EC.presence_of_element_located((By.LINK_TEXT, ("Voltar"))))
+			except:
+				bot.send_message(chat_id=update.message.chat_id, text="As notas dessa disciplina ainda não foram lançadas!")
+			else:
+				driver.save_screenshot("img/"+USERLOGIN+DISC+".png")
+				bot.send_photo(chat_id=update.message.chat_id, photo=open("img/"+USERLOGIN+DISC+".png","rb"), caption="Aqui estão suas notas!")
+	except Exception as e:
+		print(type(e))
+		print(e.args)
+		print(e)
+		bot.send_message(chat_id=update.message.chat_id, text="Sinto muito, não consegui recuperar suas notas!\nTalvez seus dados estejam incorretos, ou o SIGAA está fora do ar!")
+	driver.quit()
+
+disc_handler = CommandHandler('disc', disc, pass_args=True)
+dispatcher.add_handler(disc_handler)
+
+def help(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="Vou te ajudar a ver suas notas do SIGAA Unifei!\nComandos:\n/start - exibe a mensagem inicial\n/help - exibe esta mensagem\n/notas <usuario> <senha> - Obter as notas de todas as disciplinas\n/disc <sigla> <usuario> <senha> - Obter as notas da disciplina indicada")
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
